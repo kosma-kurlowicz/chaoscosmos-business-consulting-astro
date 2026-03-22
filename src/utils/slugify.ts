72626fecae8712609728d4b9841d0991ee27640a
Kosma Kurlowicz <kosma.kurlowicz@gmail.com>
2026-03-22 20:44:24 +0100

    Fix: Slugify with Polish Extnded Latin Characters.

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -7,6 +7,16 @@ export function slugify(text: string): string {
     return text
         .toLowerCase()
         .trim()
+        .replace(/[ś]+/g, 's')
+        .replace(/[ą]+/g, 'a')
+        .replace(/[ę]+/g, 'e')
+        .replace(/[ó]+/g, 'o')
+        .replace(/[ł]+/g, 'l')
+        .replace(/[ż]+/g, 'z')
+        .replace(/[ź]+/g, 'z')
+        .replace(/[ć]+/g, 'c')
+        .replace(/[ń]+/g, 'n')
+        .replace(/&/g, 'i')
         .replace(/[^a-z0-9]+/g, '-')
         .replace(/^-+|-+$/g, '')
 }
